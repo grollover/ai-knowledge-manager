740aa8d66b70f07023ca49a5338b42d9e98cc195
grollover <grollover.s@gmail.com>
2025-12-22 17:12:44 +0300

    build(ai-service): create vector extension for postresql

---
+++ ai-service/migrations/Version20251221201957.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251221201957 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE EXTENSION IF NOT EXISTS vector;');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP EXTENSION IF EXISTS vector;');
+    }
+}
